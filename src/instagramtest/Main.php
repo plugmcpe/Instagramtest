00b659cd3a783b2f47b1b37ce1a771250220877b
plugmcpe <M7mmdgamer7@icloud.com>
2017-07-01 15:54:43 +0300

    Update Main.php

--- src/instagramtest/Main.php
+++ src/instagramtest/Main.php
@@ -4,3 +4,9 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase{
  public function onEnable(){
   $this->getServer()->getLogger()->info("plugin has been enabled");
+  $this->getLogger()->info("hello instagram");
+ }
+  public function onDisable(){
+   $this->getServer()->getLogger()->info("plugin has been disable");
+  }
+}
